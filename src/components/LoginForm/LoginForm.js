71006cf4d00c602695b782d7544e9d50dee54a29
Jahmilli <sebastian.southern@gmail.com>
2019-10-27 13:59:15 +1100

    fixed up styling slightly

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   formLockup: {
-    backgroundColor: '#eee'
+    
   },
   form: {
     display: 'flex',
